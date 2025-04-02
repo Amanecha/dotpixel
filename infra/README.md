843e1482d2b5b97fdf473ec079c225fbafc03dd4
Amane <ur639s@icloud.com>
2025-04-03 04:41:13 +0900

    appservice name modified

--- infra/README.md
+++ infra/README.md
@@ -4,3 +4,4 @@
 ### docker run --rm -it -v $(pwd):/workspace -w /workspace run-terraform bash
 ### terraform login
 ### terraform init
+- you can use aztfexport
--- infra/main.tf
+++ infra/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "dot-app-${replace(timestamp(), "[:]", "-")}"
+  name                = "dot-app-${formatdate("YYYYMMDDhhmmss", timestamp())}"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
